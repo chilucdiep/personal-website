b6b215a5ccfca815d13df2e3dc7961ccc2a58766
chiludiep <chiludiep@hotmail.com>
2022-12-19 17:47:01 -0500

    Experience

--- components/About/Experience/Experience.tsx
+++ components/About/Experience/Experience.tsx
@@ -1,20 +1,51 @@
 import styles from "./Experience.module.scss";
-import { TechnologiesList, ToolsList, SkillsList } from "./SkillsList";
+import { TechnologiesList, ToolsList, SkillsList } from "./skillsList";
+import Coveo from "../../../assets/images/About/Coveo.svg";
+import Billmate from "../../../assets/images/About/Billmate.svg";
+import ColoredByte from "../../../assets/images/About/ColoredByte.svg";
+import Pratt from "../../../assets/images/About/Pratt.svg";
+import Poka from "../../../assets/images/About/Poka.svg";
+import Wave from "../../../assets/images/About/Wave.svg";
+import Image from "next/image";
 
 export function Experience() {
+  const skillsMarkup = (
+    <div className={styles.SkillGrid}>
+      <ListComponent listType="Technologies" list={TechnologiesList} />
+      <ListComponent listType="Tools" list={ToolsList} />
+      <ListComponent listType="Skills" list={SkillsList} />
+    </div>
+  );
+
+  const experienceMarkup = (
+    <div className={styles.Experience}>
+      <h2>I create products that have an impact.</h2>
+      <div className={styles.LogoGrid}>
+        <Image src={Poka} alt="Poka Logo" />
+        <Image src={Coveo} alt="Coveo Logo" />
+        <Image src={Billmate} alt="Billmate Logo" className={styles.Smaller} />
+        <Image src={ColoredByte} alt="ColoredByte Logo" />
+        <Image src={Pratt} alt="Pratt Logo" className={styles.Smaller} />
+      </div>
+      <Image src={Wave} alt="Wave Separator" />
+    </div>
+  );
+
   return (
     <>
-      <div className={styles.SkillGrid}>
-        <ListComponent listType="Technologies" list={TechnologiesList} />
-        <ListComponent listType="Tools" list={ToolsList} />
-        <ListComponent listType="Skills" list={SkillsList} />
-      </div>
+      {skillsMarkup}
+      {experienceMarkup}
     </>
   );
 }
 
-function ListComponent({ listType, list }: any) {
-  const listMarkup = list?.map((skill: any) => (
+interface ListProps {
+  list: string[];
+  listType: string;
+}
+
+function ListComponent({ listType, list }: ListProps) {
+  const listMarkup = list?.map((skill: string) => (
     <li key={skill}>
       <p>{skill}</p>
     </li>
