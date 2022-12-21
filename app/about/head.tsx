a88135c2092f6eb1812ca5cf4d32e02e0caf4224
chiludiep <chiludiep@hotmail.com>
2022-12-20 22:41:36 -0500

    About head

---
+++ app/about/head.tsx
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <title>Chi-Luc Diep • About</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Portfolio website and blog" />
+      <link rel="icon" href="/favicon.svg" />
+      <meta
+        property="og:image"
+        content="https://raw.githubusercontent.com/chilucdiep/Portfolio-V2/main/src/images/MainThumbnail.png"
+      />
+    </>
+  );
+}
