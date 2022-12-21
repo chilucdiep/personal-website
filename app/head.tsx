9a460d169a9cc9ff2cee06c74bd944a45942535e
chiludiep <chiludiep@hotmail.com>
2022-12-20 22:27:04 -0500

    thumbnail

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,10 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Portfolio website and blog" />
       <link rel="icon" href="/favicon.svg" />
+      <meta
+        property="og:image"
+        content="https://raw.githubusercontent.com/chilucdiep/Portfolio-V2/main/src/images/MainThumbnail.png"
+      />
     </>
   );
 }
