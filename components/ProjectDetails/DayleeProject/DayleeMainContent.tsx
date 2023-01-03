72ad8425939e6b755a5f7569c49855ac6db4f403
chiludiep <chiludiep@hotmail.com>
2023-01-03 07:55:05 -0500

    Design system section

--- components/ProjectDetails/DayleeProject/DayleeMainContent.tsx
+++ components/ProjectDetails/DayleeProject/DayleeMainContent.tsx
@@ -1,7 +1,46 @@
+import Image from "next/image";
 import { MainContent } from "../ProjectTemplate/ContentTemplate/MainContent";
 import styles from "../ProjectTemplate/ContentTemplate/MainContent/MainContent.module.scss";
+import DesignSystem from "../../../assets/images/ProjectDetails/Daylee/DesignSystem.svg";
+import TypoReturn from "../../../assets/images/ProjectDetails/Daylee/TypoReturn.svg";
+import TypoProps from "../../../assets/images/ProjectDetails/Daylee/TypoProps.svg";
+import TypoElement from "../../../assets/images/ProjectDetails/Daylee/TypoElement.svg";
+import TypoVariants from "../../../assets/images/ProjectDetails/Daylee/TypoVariants.svg";
 
 export function DayleeMainContent() {
+  const designSystemMarkup = (
+    <div className={styles.ContentSection}>
+      <h2>Design system</h2>
+      <p>
+        For better consistency throughout the app and a better developer
+        experience, I created and implemented the Daylee’s design system and
+        component library. This design system lay down the rules to follow when
+        it comes to typography, color, buttons, input, branding, and more.
+      </p>
+      <Image src={DesignSystem} alt="Design System" />
+      <p>
+        Here is a simple example of a flexible, re-usable, easy to use component
+        of the design system I implemented.
+      </p>
+      <h4>Typography Component</h4>
+      <Image src={TypoReturn} alt="Typo Return" className={styles.NoMargin} />
+      <div className={styles.ThreeColumnGrid}>
+        <div>
+          <p>Takes in props</p>
+          <Image src={TypoProps} alt="Typo Props" />
+        </div>
+        <div>
+          <p>To define element type</p>
+          <Image src={TypoElement} alt="Typo Element" />
+        </div>
+        <div>
+          <p>And the variants & styling</p>
+          <Image src={TypoVariants} alt="Typo Variants" />
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <MainContent
       projectBrief="I was responsible for designing and building the Daylee website and
@@ -16,16 +55,7 @@ export function DayleeMainContent() {
       goal="Provide an easy and intuitive way to balance work, projects, social
   life and personal growth in a all-in-one solution."
     >
-      <div className={styles.ContentSection}>
-        <h2>Design system</h2>
-        <p>
-          For better consistency throughout the app and a better developer
-          experience, I created and implemented the Daylee’s design system and
-          component library. This design system lay down the rules to follow
-          when it comes to typography, color, buttons, input, branding, and
-          more.
-        </p>
-      </div>
+      {designSystemMarkup}
     </MainContent>
   );
 }
