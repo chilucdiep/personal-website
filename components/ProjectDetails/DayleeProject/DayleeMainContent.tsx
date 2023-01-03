65bb978c2349b1a5e4d2a1d5e518d2519f15df55
chiludiep <chiludiep@hotmail.com>
2023-01-03 08:28:44 -0500

    Features section

--- components/ProjectDetails/DayleeProject/DayleeMainContent.tsx
+++ components/ProjectDetails/DayleeProject/DayleeMainContent.tsx
@@ -6,6 +6,10 @@ import TypoReturn from "../../../assets/images/ProjectDetails/Daylee/TypoReturn.
 import TypoProps from "../../../assets/images/ProjectDetails/Daylee/TypoProps.svg";
 import TypoElement from "../../../assets/images/ProjectDetails/Daylee/TypoElement.svg";
 import TypoVariants from "../../../assets/images/ProjectDetails/Daylee/TypoVariants.svg";
+import PomodoroCode from "../../../assets/images/ProjectDetails/Daylee/PomodoroCode.svg";
+import Pomodoro from "../../../assets/images/ProjectDetails/Daylee/Pomodoro.svg";
+import TodoCode from "../../../assets/images/ProjectDetails/Daylee/TodoCode.svg";
+import Todo from "../../../assets/images/ProjectDetails/Daylee/Todo.svg";
 
 export function DayleeMainContent() {
   const designSystemMarkup = (
@@ -17,26 +21,64 @@ export function DayleeMainContent() {
         component library. This design system lay down the rules to follow when
         it comes to typography, color, buttons, input, branding, and more.
       </p>
-      <Image src={DesignSystem} alt="Design System" />
+      <Image src={DesignSystem} alt="Design System" className={styles.Full} />
       <p>
         Here is a simple example of a flexible, re-usable, easy to use component
         of the design system I implemented.
       </p>
       <h4>Typography Component</h4>
-      <Image src={TypoReturn} alt="Typo Return" className={styles.NoMargin} />
+      <Image
+        src={TypoReturn}
+        alt="Typo Return"
+        className={`${styles.NoMargin} ${styles.Full}`}
+      />
       <div className={styles.ThreeColumnGrid}>
         <div>
           <p>Takes in props</p>
-          <Image src={TypoProps} alt="Typo Props" />
+          <Image src={TypoProps} alt="Typo Props" className={styles.Full} />
         </div>
         <div>
           <p>To define element type</p>
-          <Image src={TypoElement} alt="Typo Element" />
+          <Image src={TypoElement} alt="Typo Element" className={styles.Full} />
         </div>
         <div>
           <p>And the variants & styling</p>
-          <Image src={TypoVariants} alt="Typo Variants" />
+          <Image
+            src={TypoVariants}
+            alt="Typo Variants"
+            className={styles.Full}
+          />
         </div>
+      </div>
+    </div>
+  );
+
+  const featuresMarkup = (
+    <div className={styles.ContentSection}>
+      <h2>Some cool features</h2>
+      <h4>Pomodoro timer</h4>
+      <p>
+        The pomodoro timer is a time manaagement method that promotes focus time
+        with its 25 minutes work interval followed by a 5 minutes break. In
+        addition of starting and pausing the timer, this feature also keeps
+        track of the number of cycles/rounds completed paired with a goal of
+        rounds to achieve.
+      </p>
+      <div className={styles.TwoColumnGrid}>
+        <Image src={PomodoroCode} alt="Pomodoro Code" />
+        <Image src={Pomodoro} alt="Pomodoro" />
+      </div>
+      <h4>To-do list</h4>
+      <p>
+        The to-do list allows the user to plan their daily tasks with the option
+        to reflect on the day for gratitude practice or introspection. This
+        feature was implemented with the useRef hook to allow the user the
+        option to add a to-do by pressing the “Enter” key. This gives the
+        component a seemless user experience.
+      </p>
+      <div className={styles.TwoColumnGrid}>
+        <Image src={TodoCode} alt="Todo Code" />
+        <Image src={Todo} alt="Todo" />
       </div>
     </div>
   );
@@ -56,6 +98,7 @@ export function DayleeMainContent() {
   life and personal growth in a all-in-one solution."
     >
       {designSystemMarkup}
+      {featuresMarkup}
     </MainContent>
   );
 }
