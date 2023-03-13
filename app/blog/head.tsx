e457213330437e7a7883db5e6cf9452ae9028787
chiludiep <chiludiep@hotmail.com>
2023-03-13 18:38:56 -0400

    blog font

--- app/blog/head.tsx
+++ app/blog/head.tsx
@@ -9,13 +9,6 @@ export default function Head() {
         property="og:image"
         content="https://raw.githubusercontent.com/chilucdiep/Portfolio-V2/main/src/images/MainThumbnail.png"
       />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        precedence="default"
-        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
-        rel="stylesheet"
-      />
     </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { Poppins } from "@next/font/google";
+import { DM_Sans, Poppins } from "@next/font/google";
 import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
 import "./globals.scss";
 
 const poppins = Poppins({ weight: ["300", "500", "600"], subsets: ["latin"] });
+
+const dm = DM_Sans({
+  variable: "--font-inter",
+  style: ["normal", "italic"],
+  weight: ["400", "500", "700"],
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={poppins.className}>
+    <html lang="en" className={`${poppins.className}  ${dm.variable}`}>
       <head />
       <body>
         <Navbar />
